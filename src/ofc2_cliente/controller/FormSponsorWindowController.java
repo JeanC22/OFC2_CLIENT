db7b00d936abf0e267fb643b2f95caf3ed3d028a
Elias <2dam@HZ375745.TartangaLH.eus>
2023-01-25 19:57:06 +0100

    Prueba de creado

--- src/ofc2_cliente/controller/FormSponsorWindowController.java
+++ src/ofc2_cliente/controller/FormSponsorWindowController.java
@@ -7,6 +7,8 @@ package ofc2_cliente.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Collection;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.ObservableList;
@@ -27,7 +29,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import javafx.util.StringConverter;
+import ofc2_cliente.model.AdType;
 import ofc2_cliente.model.Sponsor;
 
 /**
@@ -39,7 +41,8 @@ public class FormSponsorWindowController{
     private static String regexEmail = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
             + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
     private Stage stage;
-    private Sponsor sponsor;
+    private Sponsor sp;
+    private AdType ad;
     private ObservableList<Sponsor> sponsorList;
     @FXML
     private Pane sponsorForm;
@@ -98,7 +101,6 @@ public class FormSponsorWindowController{
         stage.setOnShowing(this::windowShowing);
         returnBtn.setOnAction(this::returnSponsorWindow);
         confirmBtn.setOnAction(this::btnCreateSponsor);
-        
         //Show window
         stage.show();
 
@@ -111,10 +113,9 @@ public class FormSponsorWindowController{
         txtFPhone.setOnKeyReleased(this::enableConfirmBtn);
         txtFEmail.setOnKeyReleased(this::enableConfirmBtn);
         dpDate.setOnKeyReleased(this::enableConfirmBtn);
-        cmbAdType.getItems().addAll(sponsor.getAd());
+        cmbAdType.getItems().addAll(ad.VIDEO.toString(), 
+                ad.PANCARTA.toString(), ad.POSTER.toString());
         
-        
-    
     }
     
     @FXML
@@ -147,66 +148,72 @@ public class FormSponsorWindowController{
         try {
             if(!this.txtFName.getText().isEmpty() 
                     && !this.txtFPhone.getText().isEmpty()
-                    && !this.txtFEmail.getText().isEmpty()){
+                    && !this.txtFEmail.getText().isEmpty()
+                    && !dpDate.toString().isEmpty()){
                 confirmBtn.setDisable(false);
             }else{
                 confirmBtn.setDisable(true);
             }
         } catch (Exception e) {
-            new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK)
-                    .showAndWait();
+            e.getMessage();
         }
     }
     @FXML
     private void btnCreateSponsor(ActionEvent event) {
         try {
-            
-            if(this.txtFName.getText().length() > 30){
+
+            if (this.txtFName.getText().length() > 30) {
                 throw new Exception("La longitud maxima "
                         + "del campo Name es de 30 caracteres");
             }
-            if(!isNumeric(this.txtFPhone.getText().trim())){
+            if (!isNumeric(this.txtFPhone.getText().trim())) {
                 throw new Exception("El campo Phone no es numerico");
             }
-            
-            if(this.txtFPhone.getText().length() > 9
-                    || this.txtFPhone.getText().length()< 9){
+
+            if (this.txtFPhone.getText().length() > 9
+                    || this.txtFPhone.getText().length() < 9) {
                 throw new Exception("La longitud maxima "
                         + "del campo Phone es de 9 caracteres o minimo de 9 caracteres");
             }
-            
-            if(!this.txtFEmail.getText().matches(regexEmail)){
+
+            if (!this.txtFEmail.getText().matches(regexEmail)) {
                 throw new Exception("El campo Email no tiene el formato adecuado");
             }
-            
-            
-            try {
-                Stage mainStage = new Stage();
-                URL viewLink = getClass().getResource(
-                        "/ofc2_cliente/ui/SponsorWindow.fxml");
-                // initialition loader
-                FXMLLoader loader = new FXMLLoader(viewLink);
-                //make the root with the loader
-                Parent root = (Parent) loader.load();
-                //Get the controller
-                SponsorWindowController mainStageController
-                        = ((SponsorWindowController) loader.getController());
-                //set the stage
-                mainStageController.setStage(mainStage);
-                //start the stage
-                mainStageController.initStage(root);
-                this.stage.close();
-                event.consume();
+            String name = txtFName.getText();
+            String email = txtEmail.getText();
+            Integer phone = Integer.parseInt(txtFPhone.getText());
+            Boolean status = chbxState.isSelected();
+            Sponsor s = new Sponsor(name, phone, email, status);
+            if (!sponsorList.contains(s)) {
+                this.sp = s;
+                try {
+                    Stage mainStage = new Stage();
+                    URL viewLink = getClass().getResource(
+                            "/ofc2_cliente/ui/SponsorWindow.fxml");
+                    // initialition loader
+                    FXMLLoader loader = new FXMLLoader(viewLink);
+                    //make the root with the loader
+                    Parent root = (Parent) loader.load();
+                    //Get the controller
+                    SponsorWindowController mainStageController
+                            = ((SponsorWindowController) loader.getController());
+                    //set the stage
+                    mainStageController.setStage(mainStage);
+                    //start the stage
+                    mainStageController.initStage(root);
+                    this.stage.close();
+                    event.consume();
 
-            } catch (IOException ex) {
-                Logger.getLogger(FormSponsorWindowController.class.getName())
-                        .log(Level.SEVERE, ex.getMessage(), ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(FormSponsorWindowController.class.getName())
+                            .log(Level.SEVERE, ex.getMessage(), ex);
+                }
             }
-            
+
         } catch (Exception e) {
             new Alert(Alert.AlertType.ERROR, e.getMessage(), ButtonType.OK)
                     .showAndWait();
-            
+
         }
     }
     
@@ -221,5 +228,15 @@ public class FormSponsorWindowController{
         return true;
     }
 
+    public Sponsor getSp() {
+        return sp;
+    }
+    
+    public void lista(ObservableList<Sponsor> sponsorList){
+        this.sponsorList = sponsorList;
+    }
+    
+    
+    
     
 }
--- src/ofc2_cliente/controller/SponsorWindowController.java
+++ src/ofc2_cliente/controller/SponsorWindowController.java
@@ -7,15 +7,23 @@ package ofc2_cliente.controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
@@ -27,7 +35,11 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-import ofc2_cliente.logic.SponsorManager;
+import javax.ws.rs.core.GenericType;
+import ofc2_cliente.logic.BusinessLogicException;
+import ofc2_cliente.logic.SponsorManagerFactory;
+import ofc2_cliente.logic.SponsorRESTfulClient;
+import ofc2_cliente.model.AdType;
 import ofc2_cliente.model.Sponsor;
 
 /**
@@ -36,9 +48,9 @@ import ofc2_cliente.model.Sponsor;
  * @author Elias
  */
 public class SponsorWindowController{
-    private SponsorManager sponsor;
     private ObservableList<Sponsor> sponsorList;
     private Stage stage;
+    private AdType ad;
     @FXML
     private Pane sponsorPane;
     @FXML
@@ -79,6 +91,10 @@ public class SponsorWindowController{
     private MenuItem mItModify;
     @FXML
     private MenuItem mItDelete;
+    private Sponsor sponsor;
+    
+    SponsorManagerFactory sponsorFactory = new SponsorManagerFactory();
+    SponsorRESTfulClient rest = (SponsorRESTfulClient) sponsorFactory.createSponsorManager();
 
     public void setStage(Stage stage) {
         this.stage = stage;
@@ -101,16 +117,10 @@ public class SponsorWindowController{
         stage.setResizable(false);
         stage.setOnShowing(this::windowShowing);
         createBtn.setOnAction(this::formSponsorWindow);
+        modifyBtn.setOnAction(this::formSponsorWindow);
+        tbvSponsor.getSelectionModel().selectedItemProperty()
+                    .addListener(this::enbledButtons);
         
-        clName.setCellFactory(new PropertyValueFactory<>("name"));
-        clEmail.setCellFactory(new PropertyValueFactory<>("email"));
-        clState.setCellFactory(new PropertyValueFactory<>("status"));
-        clDate.setCellFactory(new PropertyValueFactory<>("date"));
-        clPhone.setCellFactory(new PropertyValueFactory<>("phone"));
-        clAdType.setCellFactory(new PropertyValueFactory<>("ad"));
-        clEvents.setCellFactory(new PropertyValueFactory<>("events"));
-        //sponsorList = FXCollections.observableArrayList(sponsor.findAllSponsors_XML(new GenericType<List<Sponsor>>() {}));
-        tbvSponsor.setItems(sponsorList);
         //Show window
         stage.show();
 
@@ -120,12 +130,56 @@ public class SponsorWindowController{
         createBtn.setDisable(false);
         modifyBtn.setDisable(true);
         deleteBtn.setDisable(true);
+        
+        clName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        clEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        clState.setCellValueFactory(new PropertyValueFactory<>("status"));
+        clPhone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        clAdType.setCellValueFactory(new PropertyValueFactory<>("ad"));
+        sponsorList = FXCollections.observableArrayList(sponsor = new Sponsor("TEST", 672838, "TEST", true));
+        tbvSponsor.setItems(sponsorList);
     }
     
     @FXML
     private void formSponsorWindow(ActionEvent event) {
         try {
-             Stage mainStage = new Stage();
+
+            Stage mainStage = new Stage();
+            URL viewLink = getClass().getResource(
+                    "/ofc2_cliente/ui/FormSponsorWindow.fxml");
+            // initialition loader
+            FXMLLoader loader = new FXMLLoader(viewLink);
+            //make the root with the loader
+            Parent root = (Parent) loader.load();
+            //Get the controller
+            FormSponsorWindowController mainStageController
+                    = ((FormSponsorWindowController) loader.getController());
+            mainStageController.lista(sponsorList);
+            //set the stage
+            mainStageController.setStage(mainStage);
+            //start the stage
+            mainStageController.initStage(root);
+
+            this.stage.close();
+            
+            Sponsor s = mainStageController.getSp();
+            if(s!=null){
+                this.sponsorList.add(s);
+                tbvSponsor.refresh();
+            }
+            
+
+        } catch (IOException ex) {
+            Logger.getLogger(SponsorWindowController.class.getName())
+                    .log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+    
+    @FXML
+    private void modifySponsor(ActionEvent event) {
+        try {
+
+            Stage mainStage = new Stage();
             URL viewLink = getClass().getResource(
                     "/ofc2_cliente/ui/FormSponsorWindow.fxml");
             // initialition loader
@@ -141,11 +195,40 @@ public class SponsorWindowController{
             mainStageController.initStage(root);
 
             this.stage.close();
-            
+
         } catch (IOException ex) {
             Logger.getLogger(SponsorWindowController.class.getName())
                     .log(Level.SEVERE, ex.getMessage(), ex);
         }
     }
+    /**
+     @FXML
+    private void deleteSponsor(ActionEvent event) {
+        try {
+            Sponsor sponsor = ((Sponsor) this.tbvSponsor.getSelectionModel().getSelectedItem());
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                    "¿Borrar la fila seleccionada?",
+                    ButtonType.OK, ButtonType.CANCEL);
+            Optional<ButtonType> result = alert.showAndWait();
+
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                rest.remove(sponsor.getId().toString());
+                tbvSponsor.getItems().remove(sponsor);
+                tbvSponsor.refresh();
+            }
+        } catch (BusinessLogicException b) {
+            Logger.getLogger(SponsorWindowController.class.getName()).log(Level.SEVERE, null, b);
+        }
+
+    }**/
+    
+    private void enbledButtons(ObservableValue observable, Object oldValue, Object newValue) {
+        if(newValue!=null){
+            modifyBtn.setDisable(false);
+            deleteBtn.setDisable(false);
+        }
+    }
+    
+    
 }
 
